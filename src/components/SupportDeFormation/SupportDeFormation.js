b0208d310db2237d8ea5c5a453d695183cd959e8
Wayd26 <wayedoun@gmail.com>
2022-08-15 19:32:56 +0100

    add sections of Espace DPO

--- src/components/SupportDeFormation/SupportDeFormation.js
+++ src/components/SupportDeFormation/SupportDeFormation.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const SupportDeFormation = () => {
+    const livreRow = () => {
+            return <div>
+                
+            </div>
+    }
+    const videoRow = () => {
+
+    }
+    
   return (
     <div>
         <h4 className='text-left'>Livres</h4>
+        <br />
         <h4 className='text-left'>Vidéos</h4>
     </div>
   )
