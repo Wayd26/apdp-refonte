d71f297c3e367b45ca400cca1a83cb9d78975aba
Wayd26 <wayedoun@gmail.com>
2022-09-25 12:26:33 +0100

    fix bugs

--- src/components/TemplateVideothequeCard/TemplateVideothequeCard.js
+++ src/components/TemplateVideothequeCard/TemplateVideothequeCard.js
@@ -10,10 +10,13 @@ const TemplateVideothequeCard = (props) => {
     return (
         <div className='template-videotheque-card'>            
             <iframe className="template-videotheque-card-video"
-                src={sub_title.includes("embed") ? sub_title : (`https://www.youtube.com/embed/${sub_title.slice(32, -1)+sub_title[sub_title.length - 1]}`)}
+                // src={sub_title}
+                // src={sub_title.includes("embed") ? sub_title : (`https://www.youtube.com/embed/${sub_title.slice(32, -1)+sub_title[sub_title.length - 1]}`)}
+                src={sub_title.includes("embed") ? sub_title : (`https://www.youtube.com/embed/${sub_title.slice(32, 43)}`)}
             >
             </iframe>
             <p className="template-videotheque-card-label">{title}</p>
+            
         </div>
     )
 }
